6b00dd79edd66710833fd5de949df964112e8b89
vishnu <hopefadbest@gmail.com>
2023-05-30 23:54:44 +0530

    my first commit

---
+++ src/test/java/Vishnuraj/Mavenjava/AppiumTest.java
@@ -0,0 +1,21 @@
+package Vishnuraj.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAPPandroid()
+	{
+		System.out.println("NativeAPPandroid");
+            System.out.println("NativeAPPandroid2");
+	}
+	
+
+	@Test
+	public void IOSapps()
+	{
+		System.out.println("IOSapps");
+	}
+
+}
---
+++ src/test/java/Vishnuraj/Mavenjava/RestAPITest.java
@@ -0,0 +1,22 @@
+package Vishnuraj.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void postjira()
+	{
+		System.out.println("postjira");
+		System.out.println("postjira1");
+		System.out.println("postjira2");
+	}
+	
+
+	@Test
+	public void deletetwitter()
+	{
+		System.out.println("deletetwitter");
+	}
+
+}
